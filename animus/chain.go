1f4b631d49e79e2f4863354916cc8c2b1e37c6d0
Manish Meganathan <757manish@gmail.com>
2021-05-31 11:37:40 +0530

    Add block chain persistence layer
    - Integrated the blockchain with BadgerDB, a key value memory store written in native Go.
    - Refactored the BlockChain struct to contain the reference to the database and the last hash of the chain.
    - Refactored the blockchain constructor to use the database and construct an existing chain or initialize a new chain.
    - Refactored the blockchain AddBlock method to use the database and update the chain object accordingly.

--- animus/chain.go
+++ animus/chain.go
@@ -1,26 +1,115 @@
 package animus
 
+import (
+	"log"
+
+	"github.com/dgraph-io/badger"
+)
+
+const dbpath = "./db/blocks"
+
 type BlockChain struct {
-	Blocks []*Block
+	Database *badger.DB
+	LastHash []byte
 }
 
-// A constructor function that generates a new BlockChain
-// and initializes the chain with a Genesis Block.
+// A constructor function that generates a BlockChain from the database.
+// If the blockchain does not exist on the database, a new is created
+// and initialized with a Genesis Block.
 func NewBlockChain() *BlockChain {
-	// Generate a Genesis Block for the chain
-	genesisblock := NewBlock("Genesis", []byte{})
-	// Construct a new BlockChain and add the Genesis Block to it
-	blockchain := BlockChain{Blocks: []*Block{genesisblock}}
+	// Declare a slice a bytes to collect the hash value
+	var lasthash []byte
+
+	// Open the Badger DB with the default option and db path
+	db, err := badger.Open(badger.DefaultOptions(dbpath))
+	// Handle any potential error
+	Handle(err)
+
+	// Define an Update Transaction on the BadgerDB
+	err = db.Update(func(txn *badger.Txn) error {
+
+		// Check if the last hash key 'lh' has been set in the DB
+		if item, err := txn.Get([]byte("lh")); err == badger.ErrKeyNotFound {
+			// Print that no blockchain currently exists
+			log.Println("No Existing BlockChain!")
+
+			// Generate a Genesis Block for the chain
+			genesisblock := NewBlock("Genesis", []byte{})
+			log.Println("Genesis Block Signed!")
+
+			// Add the Block to the DB with its hash as the key and its gob data as the value
+			err = txn.Set(genesisblock.Hash, BlockSerialize(genesisblock))
+			// Handle any potential error
+			Handle(err)
+
+			// Retrieve the hash of the Genesis Block
+			lasthash = genesisblock.Hash
+
+			// Set the last hash of the chain in the DB to the Genesis Block's Hash
+			err = txn.Set([]byte("lh"), lasthash)
+			return err
+
+		} else {
+
+			// Retrieve the value of the last hash item
+			err = item.Value(func(val []byte) error {
+				lasthash = val
+				return nil
+			})
+			return err
+		}
+
+	})
+	// Handle any potential error
+	Handle(err)
+
+	// Construct a blockchain with the BadgerDB and the last hash of the chain
+	blockchain := BlockChain{Database: db, LastHash: lasthash}
 	// Return the blockchain
 	return &blockchain
 }
 
-// A method of BlockChain that adds a new block to the chain
+// A method of BlockChain that adds a new Block to the chain
 func (chain *BlockChain) AddBlock(blockdata string) {
-	// Retrieve the previous block on the chain
-	prevblock := chain.Blocks[len(chain.Blocks)-1]
-	// Generate a new block from the block data and the has of the previous block
-	newblock := NewBlock(blockdata, prevblock.Hash)
-	// Add the new blocj to the chain
-	chain.Blocks = append(chain.Blocks, newblock)
+	// Declare a slice a bytes to collect the hash value
+	var lasthash []byte
+
+	// Define a View Transaction on the BadgerDB
+	err := chain.Database.View(func(txn *badger.Txn) error {
+
+		// Get the value of the last hash key in the database
+		item, err := txn.Get([]byte("lh"))
+		// Handle any potential error
+		Handle(err)
+
+		// Retrieve the value of the last hash item
+		err = item.Value(func(val []byte) error {
+			lasthash = val
+			return nil
+		})
+		return err
+	})
+
+	// Handle any potential error
+	Handle(err)
+	// Generate a new Block from the given block data and the hash of the previous block
+	block := NewBlock(blockdata, lasthash)
+
+	// Define an Update Transaction on the BadgerDB
+	err = chain.Database.Update(func(txn *badger.Txn) error {
+
+		// Add the Block to the DB with its hash as the key and its gob data as the value
+		err := txn.Set(block.Hash, BlockSerialize(block))
+		// Handle any potential error
+		Handle(err)
+
+		// Assign the hash of the block as the last hash of the chain
+		chain.LastHash = block.Hash
+		// Set the last hash key of the database to the hash of the block
+		err = txn.Set([]byte("lh"), block.Hash)
+		return err
+	})
+
+	// Handle any potential error
+	Handle(err)
 }
