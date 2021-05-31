0f762441b0e0a9ceccfa3e84c4a12894641720df
Manish Meganathan <757manish@gmail.com>
2021-05-31 10:19:56 +0530

    Update block.go
    - Added a function Handle to handle errors by logging them and then throwing a panic
    - Added serialize and deserialize functionality to the Block struct allowing to convert into a gob of bytes data and back

--- animus/block.go
+++ animus/block.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"crypto/sha256"
 	"encoding/binary"
+	"encoding/gob"
 	"fmt"
 	"log"
 	"math"
@@ -143,11 +144,48 @@ func Hexify(number int64) []byte {
 	buff := new(bytes.Buffer)
 	// Write the number as a binary into the buffer in Big Endian order
 	err := binary.Write(buff, binary.BigEndian, number)
-	if err != nil {
-		// Throw a panic if an error occurs
-		log.Panic(err)
-	}
+	// Handle any potential error
+	Handle(err)
 
 	// Return the bytes from the binary buffer
 	return buff.Bytes()
+}
+
+// A function to handle errors.
+func Handle(err error) {
+	// Check if error is non null
+	if err != nil {
+		// Log the error and throw a panic
+		log.Panic(err)
+	}
+}
+
+// A function to serialize a Block into gob of bytes
+func BlockSerialize(block *Block) []byte {
+	// Create a bytes buffer
+	var gobdata bytes.Buffer
+	// Create a new Gob encoder with the bytes buffer
+	encoder := gob.NewEncoder(&gobdata)
+	// Encode the Block into a gob
+	err := encoder.Encode(block)
+	// Handle any potential errors
+	Handle(err)
+
+	// Return the gob bytes
+	return gobdata.Bytes()
+}
+
+// A function to deserialize a gob of bytes into a Block
+func BlockDeserialize(gobdata []byte) *Block {
+	// Declare a Block variable
+	var block Block
+	// Create a new Gob decoder by reading the gob bytes
+	decoder := gob.NewDecoder(bytes.NewReader(gobdata))
+	// Decode the gob into a Block
+	err := decoder.Decode(&block)
+	// Handle any potential errors
+	Handle(err)
+
+	// Return the pointer to the Block
+	return &block
 }
