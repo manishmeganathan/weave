e95b62c0c046800a8154e104031579b6bdbfe66f
Manish Meganathan <757manish@gmail.com>
2021-06-02 13:48:29 +0530

    Update Chain initialization functions
    - Removed the NewBlockChain constructor function and replaced with two new functions
    - Added SeedBlockChain as a constructor function that creates a new blockchain if it doesnt already exist with a genesis block that contains a coinbase transaction.
    - Added AnimateBlockChain as a constructor function that re-animates an existing blockchain from the database.
    - Update the AddBlock method to use the new transactions based version of Block

--- blockchain/chain.go
+++ blockchain/chain.go
@@ -1,12 +1,11 @@
 package blockchain
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/dgraph-io/badger"
 )
-
-const dbpath = "./db/blocks"
 
 // A structure that represents the Animus BlockChain
 type BlockChain struct {
@@ -20,69 +19,89 @@ type BlockChainIterator struct {
 	Database   *badger.DB // Represents the reference to the chain database
 }
 
-// A constructor function that generates a BlockChain from the database.
-// If the blockchain does not exist on the database, a new is created
-// and initialized with a Genesis Block.
-func NewBlockChain() *BlockChain {
+// A constructor function that animates an existing blockchain i.e brings it to life.
+// Returns an error if no Animus Blockchain exists.
+func AnimateBlockChain() (*BlockChain, error) {
 	// Declare a slice a bytes to collect the hash value
 	var lasthash []byte
 
-	// Define the BadgerDB options for the DB path
-	opts := badger.DefaultOptions(dbpath)
-	// Switch off the Badger Logger
-	opts.Logger = nil
+	// Check if a blockchain already exists by checking if the DB exists
+	if !DBexists() {
+		return &BlockChain{}, fmt.Errorf("blockchain does not exist")
+	}
 
-	// Open the Badger DB with the defined options
-	db, err := badger.Open(opts)
-	// Handle any potential error
-	Handle(err)
+	// Open the BadgerDB
+	db := DBopen()
 
 	// Define an Update Transaction on the BadgerDB
-	err = db.Update(func(txn *badger.Txn) error {
+	err := db.Update(func(txn *badger.Txn) error {
+		// Get the last hash item from the DB
+		item, err := txn.Get([]byte("lh"))
+		// Handle any potential error
+		Handle(err)
 
-		// Check if the last hash key 'lh' has been set in the DB
-		if item, err := txn.Get([]byte("lh")); err == badger.ErrKeyNotFound {
-			// Print that no blockchain currently exists
-			log.Println("No Existing BlockChain!")
-
-			// Generate a Genesis Block for the chain
-			genesisblock := NewBlock("Genesis", []byte{})
-			log.Println("Genesis Block Signed!")
-
-			// Add the Block to the DB with its hash as the key and its gob data as the value
-			err = txn.Set(genesisblock.Hash, BlockSerialize(genesisblock))
-			// Handle any potential error
-			Handle(err)
-
-			// Retrieve the hash of the Genesis Block
-			lasthash = genesisblock.Hash
-
-			// Set the last hash of the chain in the DB to the Genesis Block's Hash
-			err = txn.Set([]byte("lh"), lasthash)
-			return err
-
-		} else {
-
-			// Retrieve the value of the last hash item
-			err = item.Value(func(val []byte) error {
-				lasthash = val
-				return nil
-			})
-			return err
-		}
-
+		// Retrieve the value of the last hash item
+		err = item.Value(func(val []byte) error {
+			lasthash = val
+			return nil
+		})
+		return err
 	})
-	// Handle any potential error
+
+	// Handle any potential errors
 	Handle(err)
 
 	// Construct a blockchain with the BadgerDB and the last hash of the chain
 	blockchain := BlockChain{Database: db, LastHash: lasthash}
 	// Return the blockchain
-	return &blockchain
+	return &blockchain, nil
+}
+
+// A constructor function that seeds a new blockchain i.e creates one.
+// Returns an error if an Animus Blockchain already exists.
+func SeedBlockChain(address string) (*BlockChain, error) {
+	// Declare a slice a bytes to collect the hash value
+	var lasthash []byte
+
+	// Check if a blockchain already exists by checking if the DB exists
+	if DBexists() {
+		return &BlockChain{}, fmt.Errorf("blockchain already exists exist")
+	}
+
+	// Open the BadgerDB
+	db := DBopen()
+
+	// Define an Update Transaction on the BadgerDB
+	err := db.Update(func(txn *badger.Txn) error {
+		// Generate a coinbase transaction for the genesis block
+		coinbase := NewCoinbaseTransaction(address, "First Transaction from Genesis")
+		// Generate a Genesis Block for the chain with a coinbase transaction
+		genesisblock := NewBlock([]*Transaction{coinbase}, []byte{})
+		log.Println("Genesis Block Signed!")
+
+		// Add the Block to the DB with its hash as the key and its gob data as the value
+		err := txn.Set(genesisblock.Hash, BlockSerialize(genesisblock))
+		// Handle any potential error
+		Handle(err)
+
+		// Retrieve the hash of the Genesis Block
+		lasthash = genesisblock.Hash
+		// Set the last hash of the chain in the DB to the Genesis Block's Hash
+		err = txn.Set([]byte("lh"), lasthash)
+		return err
+	})
+
+	// Handle any potential errors
+	Handle(err)
+
+	// Construct a blockchain with the BadgerDB and the last hash of the chain
+	blockchain := BlockChain{Database: db, LastHash: lasthash}
+	// Return the blockchain
+	return &blockchain, nil
 }
 
 // A method of BlockChain that adds a new Block to the chain
-func (chain *BlockChain) AddBlock(blockdata string) {
+func (chain *BlockChain) AddBlock(blocktxns []*Transaction) {
 	// Declare a slice a bytes to collect the hash value
 	var lasthash []byte
 
@@ -104,8 +123,8 @@ func (chain *BlockChain) AddBlock(blockdata string) {
 
 	// Handle any potential error
 	Handle(err)
-	// Generate a new Block from the given block data and the hash of the previous block
-	block := NewBlock(blockdata, lasthash)
+	// Generate a new Block from the given block transactions and the hash of the previous block
+	block := NewBlock(blocktxns, lasthash)
 
 	// Define an Update Transaction on the BadgerDB
 	err = chain.Database.Update(func(txn *badger.Txn) error {
