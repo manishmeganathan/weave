b262d27fbe9b5c70b1ab3059934b33140acf41a4
Manish Meganathan <757manish@gmail.com>
2021-06-02 16:20:41 +0530

    Add a constructor for regular Transactions
    Added a constructor NewTransaction that accepts a to and from address and an amount and returns a Transaction object for it.

--- blockchain/transaction.go
+++ blockchain/transaction.go
@@ -4,7 +4,9 @@ import (
 	"bytes"
 	"crypto/sha256"
 	"encoding/gob"
+	"encoding/hex"
 	"fmt"
+	"log"
 )
 
 // A structure that represents a transaction on the Animus Blockchain
@@ -28,7 +30,52 @@ type TxOutput struct {
 	PublicKey string // Represents the public key of the transaction output required to retrieve the value inside it
 }
 
-func NewTransaction() {}
+// A constructor function that generates and returns a Transaction
+// given the to and from addresses and the amount to transact.
+func NewTransaction(from, to string, amount int, chain *BlockChain) *Transaction {
+	// Declare slices of transaction outputs and inputs
+	var txinputs []TxInput
+	var txoutputs []TxOutput
+	// Accumulate the spendable transaction outputs of the account up to the given amount
+	accumulated, validoutputs := chain.AccumulateSpendableTXO(from, amount)
+
+	// Check if the account has enough funds
+	if accumulated < amount {
+		log.Panic("Error: Insufficient Funds!")
+	}
+
+	// Iterate over the spendable transaction output IDs
+	for txnid, outputs := range validoutputs {
+		// Decode the transaction ID
+		txid, err := hex.DecodeString(txnid)
+		// Handle any potential error
+		Handle(err)
+
+		// Iterate over the the output indexes
+		for _, output := range outputs {
+			// Create a transaction input with the transaction ID, output index and from address signature
+			input := TxInput{ID: txid, OutIndex: output, Signature: from}
+			// Add the transaction input into the slice
+			txinputs = append(txinputs, input)
+		}
+	}
+
+	// Add a transaction output with the amount to the address
+	txoutputs = append(txoutputs, TxOutput{Value: amount, PublicKey: to})
+
+	// Check if there is a balance in the accumulated amounted
+	if accumulated > amount {
+		// Add a transaction output with the balance amount back to the original sender
+		txoutputs = append(txoutputs, TxOutput{Value: accumulated - amount, PublicKey: from})
+	}
+
+	// Create a Transaction with the list of input and outputs
+	txn := Transaction{ID: nil, Inputs: txinputs, Outputs: txoutputs}
+	// Generate an ID for the transaction
+	txn.GenerateID()
+	// Return the transaction
+	return &txn
+}
 
 // A method of Transaction that generates assigns the hash of transaction to the ID field.
 // The hash is obtained from the Gob data of the transaction.
