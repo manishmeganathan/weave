c03a4b9d270f8645477b9322a2d66a1c4230d80e
Manish Meganathan <757manish@gmail.com>
2021-06-09 02:18:40 +0530

    Add block and consensus Header structures
    - Added a ConsensusHeader interface that requires the Mint and Validate method. Delaying closing this issue until pow is successfully implemented with this model. #6
    - Added a BlockHeader structure that represents the data of the Block that is used for the hash. of the block. #2. Delaying closing this until the Compose method for the structure has been implemented.
    - Reimplemented the Block struct from the previous iteration to be more general and have more fields to support p2p layering down the line.

--- src/primitives/block.go
+++ src/primitives/block.go
@@ -4,11 +4,53 @@ of the Block structure and its methods
 */
 package primitives
 
-// A structure that represents a single Block on the Animus BlockChain
-type Block struct {
-	Hash         []byte         // Represents the hash of the Block
-	PrevHash     []byte         // Represents the hash of the previous Block
-	Transactions []*Transaction // Represents the transaction on the Block
-	Nonce        int            // Represents the nonce number that signed the block
-	Difficulty   int            // Represent the difficulty value to sign the block
+var version = []byte("v0.1.0")
+
+type ConsensusHeader interface {
+	Mint(*Block) error
+	Validate(*Block) bool
 }
+
+// A structure that represents the header of a Block
+type BlockHeader struct {
+	// Represents the consensus parameters of the Block
+	Consensus ConsensusHeader
+
+	// Represents the hash of the previous Block
+	Priori Hash
+
+	// Represents the timestamp of the Block at the point of creation
+	Timestamp uint64
+
+	// Represent the merkle root of transactions on the Block
+	MerkleRoot Hash
+
+	// Represents the weave network of the Block
+	WeaveNet []byte
+
+	// Represents the software version of Block Generator
+	Version []byte
+}
+
+// A structure that represents a single Block on the Blockchain
+type Block struct {
+	// Represents the Block Header
+	Header BlockHeader
+
+	// Represents the Block Height
+	BlockHeight uint64
+
+	// Represents the no.of Transactions in the Block
+	TXCount uint
+
+	// Represents the list of Transactions in the Block
+	TXList []*Transaction
+
+	// Represents the address of the Block origin (coinbase address)
+	BlockOrigin Address
+
+	// Represents the Hash of the Block Header
+	BlockHash Hash
+}
+
+func NewBlock() {}
