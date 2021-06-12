957cc103ed8c051c95a642d22c4b1dbd0feb2bfa
Manish Meganathan <757manish@gmail.com>
2021-06-13 02:16:11 +0530

    Add walletDB constant

--- src/utils/constants.go
+++ src/utils/constants.go
@@ -6,6 +6,7 @@ const WeaverVersion = "v0.5.0"
 // TODO: MOVE TO A LOCATION OUTSIDE OF THE REPO DIR
 const DBfile string = "./tmp/db/blocks/MANIFEST"
 const DBpath string = "./tmp/db/blocks"
+const WalletDB = "./tmp/walletstore.data"
 
 // Represents the Proof Of Work weave net ID
 const WeavePOW = "fustian"
