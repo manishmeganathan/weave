b979a1d5ddc99e2238f1039c7db89b8402c303a5
Manish Meganathan <757manish@gmail.com>
2021-06-13 14:26:43 +0530

    Update Blockchain structure and constructors
    - Renamed chain fields to chain head and chain height
    - Updated the error handling flow of the SeedBlockChain constructor
    - Added a constructor AnimateBlockChain for starting existing chains from the database

--- src/core/chain.go
+++ src/core/chain.go
@@ -10,15 +10,19 @@ import (
 )
 
 type BlockChain struct {
-	DB            *Database
-	LastHash      primitives.Hash
-	CurrentHeight int
+	// Represents the database where the chain is stored
+	DB *DatabaseClient
+
+	// Represents the hash of the latest block
+	ChainHead primitives.Hash
+
+	// Represents the number of block on the chain (last block height+1)
+	ChainHeight int
 }
 
+// A constructor function that seeds a new blockchain i.e creates one.
+// Returns an error if an Animus Blockchain already exists.
 func SeedBlockChain(address primitives.Address) (*BlockChain, error) {
-	// Declare a slice a bytes to collect the hash value
-	var lasthash primitives.Hash
-
 	// Check if a blockchain already exists by checking if the DB exists
 	if DBExists() {
 		return &BlockChain{}, fmt.Errorf("blockchain already exists exist")
@@ -27,7 +31,7 @@ func SeedBlockChain(address primitives.Address) (*BlockChain, error) {
 	// Create a null blockchain
 	blockchain := BlockChain{}
 	// Set up the database client
-	blockchain.DB = NewDatabase()
+	blockchain.DB = NewDatabaseClient()
 
 	// Generate a coinbase transaction for the genesis block
 	coinbase := NewCoinbaseTransaction(address)
@@ -40,35 +44,113 @@ func SeedBlockChain(address primitives.Address) (*BlockChain, error) {
 
 	// Generate a Genesis Block for the chain with a coinbase transaction
 	genesisblock := NewBlock(merkle, []byte{}, 0, address, []byte(utils.WeavePOW))
-
+	// Log the minting of the genesis block
 	logrus.WithFields(logrus.Fields{
 		"address": address.String,
 		"reward":  coinbase.Outputs[0].Value,
 	}).Info("genesis block has been minted!")
 
 	// Retrieve the hash of the Genesis Block
-	lasthash = genesisblock.BlockHash
+	chainhead := genesisblock.BlockHash
 
 	// Define an Update Transaction on the BadgerDB
 	err := blockchain.DB.Client.Update(func(txn *badger.Txn) error {
 
 		// Add the Block to the DB with its hash as the key and its gob data as the value
-		err := txn.Set(genesisblock.BlockHash, primitives.BlockSerialize(genesisblock))
-		// Handle any potential error
-		logrus.Fatal("genesis block could not be stored!", err)
+		if err := txn.Set(genesisblock.BlockHash, primitives.BlockSerialize(genesisblock)); err != nil {
+			// Return any potential error
+			return fmt.Errorf("genesis block could not be stored! error - %v", err)
+		}
 
-		// Set the last hash of the chain in the DB to the Genesis Block's Hash
-		err = txn.Set([]byte("lh"), lasthash)
-		return err
+		// Set the chain head of the chain in the DB to the hash of the genesis block
+		if err := txn.Set([]byte("chainhead"), chainhead); err != nil {
+			// Return any potential error
+			return fmt.Errorf("chain head could not be stored! error - %v", err)
+		}
+
+		// Set the height of the chain in the DB as 1 (genesis block height + 1)
+		if err := txn.Set([]byte("chainheight"), utils.HexEncode(1)); err != nil {
+			// Return any potential error
+			return fmt.Errorf("chain height could not be stored! error - %v", err)
+		}
+
+		return nil
 	})
+	// Handle any potential errors
+	utils.HandleErrorLog(err, "chain seed failed!")
 
-	// Log the error
-	logrus.Fatal("chain failed to be seeded!", err)
+	// Assign the current chain head
+	blockchain.ChainHead = chainhead
+	// Assign the current chain height
+	blockchain.ChainHeight = 1
 
-	// Assign the last hash of the chain
-	blockchain.LastHash = lasthash
-	// Assign the current height of the chain
-	blockchain.CurrentHeight = 0
+	// Return the blockchain
+	return &blockchain, nil
+}
+
+// A constructor function that animates an existing blockchain i.e brings it to life.
+// Returns an error if no Animus Blockchain exists.
+func AnimateBlockChain() (*BlockChain, error) {
+	// Declare a slice a bytes to collect the chain head and height
+	var chainhead []byte
+	var chainheight []byte
+
+	// Check if a blockchain already exists by checking if the DB exists
+	if !DBExists() {
+		return &BlockChain{}, fmt.Errorf("blockchain does not exist")
+	}
+
+	// Create a null blockchain
+	blockchain := BlockChain{}
+	// Set up the database client
+	blockchain.DB = NewDatabaseClient()
+
+	// Define an Update Transaction on the BadgerDB
+	err := blockchain.DB.Client.Update(func(txn *badger.Txn) error {
+
+		// Get the chain head item from the DB
+		chainheaditem, err := txn.Get([]byte("chainhead"))
+		// Return any potential error
+		if err != nil {
+			return fmt.Errorf("chain head could not be retrived! error - %v", err)
+		}
+
+		// Retrieve the value of the chain head hash item
+		if err = chainheaditem.Value(func(val []byte) error {
+			chainhead = val
+			return nil
+
+		}); err != nil {
+			// Return any potential error
+			return fmt.Errorf("chain head could not be set! error - %v", err)
+		}
+
+		// Get the chain height item from the DB
+		chainheightitem, err := txn.Get([]byte("chainheight"))
+		// Return any potential error
+		if err != nil {
+			return fmt.Errorf("chain height could not be retrived! error - %v", err)
+		}
+
+		// Retrieve the value of the chain head hash item
+		if err = chainheightitem.Value(func(val []byte) error {
+			chainhead = val
+			return nil
+
+		}); err != nil {
+			// Return any potential error
+			return fmt.Errorf("chain height could not be set! error - %v", err)
+		}
+
+		return nil
+	})
+	// Handle any potential errors
+	utils.HandleErrorLog(err, "chain animate failed!")
+
+	// Assign the current chain head
+	blockchain.ChainHead = chainhead
+	// Assign the current chain height
+	blockchain.ChainHeight = utils.HexDecode(chainheight)
 
 	// Return the blockchain
 	return &blockchain, nil
