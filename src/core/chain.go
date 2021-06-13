53ef278867f276e775afcb0f153ecb97b9904a66
Manish Meganathan <757manish@gmail.com>
2021-06-13 17:06:42 +0530

    Update chain functionality
    - Ported over the add block functionality for the chain struct and updated it to use the new tree builder method

--- src/core/chain.go
+++ src/core/chain.go
@@ -9,6 +9,7 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+// A structure that represents the blockchain
 type BlockChain struct {
 	// Represents the database where the chain is stored
 	DB *DatabaseClient
@@ -36,10 +37,13 @@ func SeedBlockChain(address primitives.Address) (*BlockChain, error) {
 	// Generate a coinbase transaction for the genesis block
 	coinbase := NewCoinbaseTransaction(address)
 
+	// Create a merkle builder
 	merkle := NewMerkleBuilder()
+	// Start the merkle builder
 	go merkle.Build()
-
+	// Send the coinbase transaction to the merkle build queue
 	merkle.BuildQueue <- coinbase
+	// Close the build queue
 	close(merkle.BuildQueue)
 
 	// Generate a Genesis Block for the chain with a coinbase transaction
@@ -154,4 +158,57 @@ func AnimateBlockChain() (*BlockChain, error) {
 
 	// Return the blockchain
 	return &blockchain, nil
+}
+
+// A method of BlockChain that adds a new Block to the chain and returns it
+func (chain *BlockChain) AddBlock(blocktxns []*primitives.Transaction, addr primitives.Address) *primitives.Block {
+	// Create a merkle builder
+	merkle := NewMerkleBuilder()
+	// Start the merkle builder
+	go merkle.Build()
+
+	// Iterate over the block transactions
+	for _, txn := range blocktxns {
+		// Send each transaction to the merkle build queue
+		merkle.BuildQueue <- txn
+	}
+	// Close the build queue
+	close(merkle.BuildQueue)
+
+	// Generate a new Block
+	block := NewBlock(merkle, chain.ChainHead, chain.ChainHeight, addr, []byte(utils.WeavePOW))
+
+	// Assign the hash of the block as the chain head
+	chain.ChainHead = block.BlockHash
+	// Increment the chain height
+	chain.ChainHeight++
+
+	// Define an Update Transaction on the BadgerDB
+	err := chain.DB.Client.Update(func(txn *badger.Txn) error {
+
+		// Add the Block to the DB with its hash as the key and its gob data as the value
+		if err := txn.Set(block.BlockHash, primitives.BlockSerialize(block)); err != nil {
+			// Return any potential error
+			return fmt.Errorf("block data could not be stored! error - %v", err)
+		}
+
+		// Set the last hash key of the database to the hash of the block
+		if err := txn.Set([]byte("chainhead"), chain.ChainHead); err != nil {
+			// Return any potential error
+			return fmt.Errorf("chain head could not be updated! error - %v", err)
+		}
+
+		// Set the last hash key of the database to the hash of the block
+		if err := txn.Set([]byte("chainheight"), utils.HexEncode(chain.ChainHeight)); err != nil {
+			// Return any potential error
+			return fmt.Errorf("chain height could not be updated! error - %v", err)
+		}
+
+		return nil
+	})
+	// Handle any potential errors
+	utils.HandleErrorLog(err, "block addition failed!")
+
+	// Return the block
+	return block
 }
