48aec7d51b4893765497b4939c4d20e13479b28a
Manish Meganathan <meganathan.manish@gmail.com>
2021-07-31 13:57:15 +0530

    Update entrypoint
    - Removed all code that invoke the command line

--- blockweave.go
+++ blockweave.go
@@ -1,18 +1,9 @@
 package main
 
-import (
-	"log"
-	"math/rand"
-	"os"
-	"time"
-
-	"github.com/manishmeganathan/blockweave/cmd"
-)
-
 func main() {
-	defer os.Exit(0)
-	rand.Seed(time.Now().UTC().UnixNano())
-	log.SetPrefix("animus ")
+	// defer os.Exit(0)
+	// rand.Seed(time.Now().UTC().UnixNano())
+	// log.SetPrefix("animus ")
 
-	cmd.Execute()
+	// cmd.Execute()
 }
