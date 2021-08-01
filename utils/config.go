0900e5e8cc5c95fafdc76b71afa3402d3684e47e
Manish Meganathan <meganathan.manish@gmail.com>
2021-08-02 04:50:25 +0530

    Update Config utilities
    - Add config variables for chain head and chain height keys
    - Updated the Config structure, added structs jbokconfig, dbconfig and bucketconfig
    - Added a function getconfigdir that returns the path to the config directory
    - Added functions CreateDirectory and ClearDirectory to add and remove directories
    - Updated GenerateConfigFile to use the new config structure and utility functions

--- utils/config.go
+++ utils/config.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"path/filepath"
 
 	"github.com/mitchellh/go-homedir"
 	"github.com/sirupsen/logrus"
@@ -13,21 +14,51 @@ import (
 // Represents the version of the source code
 const SrcVersion = "0.5.0"
 
-// Represents the prefix key used for utxo keys
-var UTXOprefix = []byte("utxo-")
+var (
+	// Represents the prefix key used for utxo keys
+	UTXOprefix = []byte("utxo-")
+	// Represents the key used for storing chain head
+	ChainHeadKey = []byte("chainhead")
+	// Represents the key used for storing the chain height
+	ChainHeightKey = []byte("chainheight")
+)
 
-// A struct that represents a config file and its data
+// A struct that represents the contents of the config file.
 type Config struct {
-	// Represents the path to the JBOK wallet data file
-	JBOKFile string `json:"jbokfile"`
-	// Represents the path to the BadgerDB manifest file
-	DBFile string `json:"dbfile"`
-	// Represents the path to the chain database directory
-	DBDirectory string `json:"dbdir"`
+	// Represents the jbok configuration
+	JBOK jbokconfig `json:"jbok"`
+	// Represents the database configuration
+	DB dbconfig `json:"db"`
+}
+
+// A struct that represents a jbok configuration
+type jbokconfig struct {
+	// Represents the path to the JBOK data file
+	File string `json:"file"`
+	// Represents the default address used for mining rewards
+	Default string `json:"default"`
+}
+
+// A struct that represents a database configuration
+type dbconfig struct {
+	// Represents the path to the database directory
+	Root string `json:"root"`
+	// Represents the configuration of the State bucket
+	State bucketconfig `json:"state"`
+	// Represents the configuration of the Blocks bucket
+	Blocks bucketconfig `json:"blocks"`
+}
+
+// A struct that represents a database bucket configuration
+type bucketconfig struct {
+	// Represents the path to the bucket manifest file
+	File string `json:"file"`
+	// Represents the path to the bucket directory
+	Directory string `json:"directory"`
 }
 
 // A function that returns the path to the config file.
-// The config file is stored at %HOME%/blockweave/config.json
+// The config file is at %HOME%/blockweave/config.json
 func getconfigpath() string {
 	// Retrieve the path to the config file.
 	filelocation, err := homedir.Expand("~/blockweave/config.json")
@@ -38,6 +69,46 @@ func getconfigpath() string {
 
 	// Return the file location
 	return filelocation
+}
+
+// A function that returns the path to the config directory.
+// The config directory is at %HOME%/blockweave/
+func getconfigdir() string {
+	// Create the path to the blockweave directory
+	configdir, err := homedir.Expand("~/blockweave")
+	if err != nil {
+		// Log a fatal error
+		logrus.WithFields(logrus.Fields{"error": err}).Fatalln("failed to detect home directory.")
+	}
+
+	// Return the path to the blockweave directory
+	return configdir
+}
+
+// A function to create a new directory given the path to the directory.
+// If the directory already exists, this is a no-op.
+func CreateDirectory(dirpath string) {
+	// Check if the directory exists
+	_, err := os.Stat(dirpath)
+	if os.IsNotExist(err) {
+		// Create the directory
+		err = os.MkdirAll(dirpath, 0755)
+		if err != nil {
+			// Log a fatal error
+			logrus.WithFields(logrus.Fields{"error": err}).Fatalln("failed to create directory: %v.", dirpath)
+		}
+	}
+}
+
+// A function to clear the contents of a directory given the path to the directory.
+// If the directory does not exist, this is a no-op.
+func ClearDirectory(dirpath string) {
+	// Remove all contents of the directory
+	err := os.RemoveAll(dirpath)
+	if err != nil {
+		// Log a fatal error
+		logrus.WithFields(logrus.Fields{"error": err}).Fatalln("failed to clear directory.")
+	}
 }
 
 // A function that checks if the config file exists in the expected location.
@@ -107,33 +178,32 @@ func WriteConfigFile(config Config) error {
 // If the file already exists, it will be overwritten.
 // If the blockweave directory does not exist, it will be created.
 func GenerateConfigFile() {
-	// Create the path to the blockweave directory
-	configdir, err := homedir.Expand("~/blockweave")
-	if err != nil {
-		// Log a fatal error
-		logrus.WithFields(logrus.Fields{"error": err}).Fatalln("failed to detect home directory.")
-	}
-
-	// Check if the blockweave directory exists
-	_, err = os.Stat(configdir)
-	if os.IsNotExist(err) {
-		// Create the blockweave directory
-		err = os.MkdirAll(configdir, 0755)
-		if err != nil {
-			// Log a fatal error
-			logrus.WithFields(logrus.Fields{"error": err}).Fatalln("failed to create blockweave directory.")
-		}
-	}
+	// Get the path to the blockweave directory.
+	configdir := getconfigdir()
+	// Create the blockweage directory if it does not exist.
+	CreateDirectory(configdir)
 
 	// Generate a default Config with default values.
-	defaultConfig := Config{
-		JBOKFile:    configdir + "/jbok.data",
-		DBFile:      configdir + "/db/manifest.json",
-		DBDirectory: configdir + "/db/",
+	defaultconfig := Config{
+		JBOK: jbokconfig{
+			File:    filepath.Join(configdir, "jbok.data"),
+			Default: "",
+		},
+		DB: dbconfig{
+			Root: filepath.Join(configdir, "db"),
+			State: bucketconfig{
+				File:      filepath.Join(configdir, "db", "state", "MANIFEST"),
+				Directory: filepath.Join(configdir, "db", "state"),
+			},
+			Blocks: bucketconfig{
+				File:      filepath.Join(configdir, "db", "blocks", "MANIFEST"),
+				Directory: filepath.Join(configdir, "db", "blocks"),
+			},
+		},
 	}
 
 	// Write the generated config and check for errors.
-	err = WriteConfigFile(defaultConfig)
+	err := WriteConfigFile(defaultconfig)
 	if err != nil {
 		// Log a fatal error
 		logrus.WithFields(logrus.Fields{"error": err}).Fatalln("failed to write generated config file.")
