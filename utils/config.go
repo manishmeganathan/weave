867e025b3607439e4f162d8597554deaa914421a
Manish Meganathan <meganathan.manish@gmail.com>
2021-08-03 00:29:30 +0530

    Update config methods and cleanup module
    - Removed directory methods (implemented in dir.go)
    - Renamed getconfigpath to getconfigfilepath
    - Converted WriteConfigFile to method of Config
    - Added a write argument for GenerateConfigFile. Method has been refactored to also return the default generated config.

--- utils/config.go
+++ utils/config.go
@@ -7,7 +7,6 @@ import (
 	"os"
 	"path/filepath"
 
-	"github.com/mitchellh/go-homedir"
 	"github.com/sirupsen/logrus"
 )
 
@@ -59,63 +58,18 @@ type bucketconfig struct {
 
 // A function that returns the path to the config file.
 // The config file is at %HOME%/blockweave/config.json
-func getconfigpath() string {
-	// Retrieve the path to the config file.
-	filelocation, err := homedir.Expand("~/blockweave/config.json")
-	if err != nil {
-		// Log a fatal error
-		logrus.WithFields(logrus.Fields{"error": err}).Fatalln("failed to detect home directory.")
-	}
-
+func getconfigfilepath() string {
+	// Retrieve the path to the config dir.
+	configdir := ConfigDirectory()
 	// Return the file location
-	return filelocation
-}
-
-// A function that returns the path to the config directory.
-// The config directory is at %HOME%/blockweave/
-func getconfigdir() string {
-	// Create the path to the blockweave directory
-	configdir, err := homedir.Expand("~/blockweave")
-	if err != nil {
-		// Log a fatal error
-		logrus.WithFields(logrus.Fields{"error": err}).Fatalln("failed to detect home directory.")
-	}
-
-	// Return the path to the blockweave directory
-	return configdir
-}
-
-// A function to create a new directory given the path to the directory.
-// If the directory already exists, this is a no-op.
-func CreateDirectory(dirpath string) {
-	// Check if the directory exists
-	_, err := os.Stat(dirpath)
-	if os.IsNotExist(err) {
-		// Create the directory
-		err = os.MkdirAll(dirpath, 0755)
-		if err != nil {
-			// Log a fatal error
-			logrus.WithFields(logrus.Fields{"error": err}).Fatalln("failed to create directory: %v.", dirpath)
-		}
-	}
-}
-
-// A function to clear the contents of a directory given the path to the directory.
-// If the directory does not exist, this is a no-op.
-func ClearDirectory(dirpath string) {
-	// Remove all contents of the directory
-	err := os.RemoveAll(dirpath)
-	if err != nil {
-		// Log a fatal error
-		logrus.WithFields(logrus.Fields{"error": err}).Fatalln("failed to clear directory.")
-	}
+	return filepath.Join(configdir, "config.json")
 }
 
 // A function that checks if the config file exists in the expected location.
 // Returns an error if the file does not exist.
 func CheckConfigFile() error {
 	// Get the path to the config file.
-	filelocation := getconfigpath()
+	filelocation := getconfigfilepath()
 
 	// Check if the file exists at the location
 	if _, err := os.Stat(filelocation); err == nil {
@@ -131,9 +85,9 @@ func CheckConfigFile() error {
 }
 
 // A function that reads the config file and returns the data as a Config object.
-func ReadConfigFile() Config {
+func ReadConfigFile() *Config {
 	// Get the path to the config file.
-	filelocation := getconfigpath()
+	filelocation := getconfigfilepath()
 
 	// Open the config file
 	configfile, err := os.Open(filelocation)
@@ -151,35 +105,17 @@ func ReadConfigFile() Config {
 
 	// Unmarshal the JSON byte array into a struct and return it
 	json.Unmarshal([]byte(byteValue), &config)
-	return config
+	return &config
 }
 
-// A function that writes a given Config object to the config file.
-// If the file already exists, it will be overwritten.
-func WriteConfigFile(config Config) error {
-	// Get the path to the config file.
-	filelocation := getconfigpath()
-
-	// Format and indent the config object provided into a byte array.
-	file, err := json.MarshalIndent(config, "", " ")
-	if err != nil {
-		return fmt.Errorf("could not format and marshal config. %v", err)
-	}
-
-	// Write the byte array to the file location.
-	if err = ioutil.WriteFile(filelocation, file, 0644); err != nil {
-		return fmt.Errorf("could not write config. %v", err)
-	}
-
-	return nil
-}
-
-// A function that generates a new config file with the default values.
+// A function that generates a new config file with the default
+// values, writes it to the file and returns it as a Config object.
+//
 // If the file already exists, it will be overwritten.
 // If the blockweave directory does not exist, it will be created.
-func GenerateConfigFile() {
+func GenerateConfigFile(write bool) *Config {
 	// Get the path to the blockweave directory.
-	configdir := getconfigdir()
+	configdir := ConfigDirectory()
 	// Create the blockweage directory if it does not exist.
 	CreateDirectory(configdir)
 
@@ -202,10 +138,36 @@ func GenerateConfigFile() {
 		},
 	}
 
-	// Write the generated config and check for errors.
-	err := WriteConfigFile(defaultconfig)
-	if err != nil {
-		// Log a fatal error
-		logrus.WithFields(logrus.Fields{"error": err}).Fatalln("failed to write generated config file.")
+	// Check if write flag is set
+	if write {
+		// Write the generated config and check for errors.
+		err := defaultconfig.WriteConfigFile()
+		if err != nil {
+			// Log a fatal error
+			logrus.WithFields(logrus.Fields{"error": err}).Fatalln("failed to write generated config file.")
+		}
 	}
+
+	// Return the default config
+	return &defaultconfig
+}
+
+// A method of Config that write the config data to the file.
+// If the file already exists, it will be overwritten.
+func (config *Config) WriteConfigFile() error {
+	// Get the path to the config file.
+	filelocation := getconfigfilepath()
+
+	// Format and indent the config object provided into a byte array.
+	file, err := json.MarshalIndent(config, "", " ")
+	if err != nil {
+		return fmt.Errorf("could not format and marshal config. %v", err)
+	}
+
+	// Write the byte array to the file location.
+	if err = ioutil.WriteFile(filelocation, file, 0644); err != nil {
+		return fmt.Errorf("could not write config. %v", err)
+	}
+
+	return nil
 }
