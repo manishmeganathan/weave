33c889654fb93170fb76abff60921b9197485c1d
Manish Meganathan <meganathan.manish@gmail.com>
2021-08-02 17:29:49 +0530

    Update chain iterator
    - Update the Database field to Blocks
    - Use the NullBlock constructor while declaring empty block in the iterator Next method

--- core/iterator.go
+++ core/iterator.go
@@ -13,26 +13,26 @@ type BlockChainIterator struct {
 	// Represents the hash of the block that the iterator is currently on
 	Cursor []byte
 	// Represents the reference to the chain blocks database bucket
-	Database *persistence.DatabaseBucket
+	Blocks *persistence.DatabaseBucket
 }
 
 // A constructor function that generates and returns an iterator for the BlockChain
 func NewIterator(chain *BlockChain) *BlockChainIterator {
 	// Assign the values of the BlockChainIterator from the chain and return it
 	return &BlockChainIterator{
-		Cursor:   chain.ChainHead,
-		Database: chain.Blocks,
+		Cursor: chain.ChainHead,
+		Blocks: chain.Blocks,
 	}
 }
 
 // A method of BlockChainIterator that iterates over chain and returns the
 // next block on the chain (backwards) from the chain DB and returns it
 func (iter *BlockChainIterator) Next() *Block {
-	// Declare a Block variable
-	var block Block
+	// Create null Block object
+	block := NullBlock()
 
 	// Define a View Transaction on the BadgerDB
-	err := iter.Database.Client.View(func(txn *badger.Txn) error {
+	err := iter.Blocks.Client.View(func(txn *badger.Txn) error {
 
 		// Get the block item for the current hash of the iterator
 		item, err := txn.Get(iter.Cursor)
@@ -54,9 +54,8 @@ func (iter *BlockChainIterator) Next() *Block {
 		}
 
 		// Convert the block gob data into a Block object
-		block = Block{}
 		block.Deserialize(blockgob)
-
+		// Return any potential error
 		return err
 	})
 
@@ -69,5 +68,5 @@ func (iter *BlockChainIterator) Next() *Block {
 	// Update the iterator's cursor to the hash of block before the current block
 	iter.Cursor = block.BlockHeader.Priori
 	// Return the block
-	return &block
+	return block
 }
