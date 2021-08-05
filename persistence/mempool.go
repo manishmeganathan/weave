50f5607e40c799a10b4c9ca1e1887709d54511f6
Manish Meganathan <meganathan.manish@gmail.com>
2021-08-05 23:58:00 +0530

    Add Put and Sizing methods for MemPool
    - Refactored PoolEvent to be of type string.
    - Renamed size to Count (public)
    - Renamed sizelimit to size.
    - Removed the events boolean
    - Updated the constructor for new MemPool fields

--- persistence/mempool.go
+++ persistence/mempool.go
@@ -1,20 +1,22 @@
 package persistence
 
-import "sync"
+import (
+	"sync"
+)
 
 // A type alias that represents a type of pool state event
-type PoolEvent uint
+type PoolEvent string
 
 // A set of constants that represent valids types of pool state events
 const (
 	// Pool is full
-	POOLFULL PoolEvent = 0
+	POOLFULL PoolEvent = "pool is full"
 	// Pool is empty
-	POOLEMPTY PoolEvent = 1
+	POOLEMPTY PoolEvent = "pool is empty"
 	// Pool has been reset
-	POOLPURGE PoolEvent = 2
+	POOLPURGE PoolEvent = "pool has been purged"
 	// Pool has been resized
-	POOLRESIZE PoolEvent = 3
+	POOLRESIZE PoolEvent = "pool has been resized"
 )
 
 // A struct that represents a memory pool of objects
@@ -24,34 +26,30 @@ type MemPool struct {
 	// Represents the syncrhonization lock for the pool
 	mutex sync.Mutex
 
-	// Represents the current size of the pool
-	size uint
+	// Represents the current number of object in the pool
+	Count uint
 	// Represents the maximum size of the pool
-	sizelimit uint
+	size uint
 
-	// Represents the event handler chanel for pool state events
+	// Represents the event channel for pool state events
 	eventchan chan PoolEvent
-	// Represents whether the the event handler is enabled.
-	events bool
 }
 
 // A constructor function that creates a new memory pool of the given size and returns it.
 // Requires a boolean that determines if the event handler for the pool should be initialized.
-func NewMemPool(poolsize uint, eventenable bool) *MemPool {
+func NewMemPool(poolsize uint, enable_events bool) *MemPool {
 	// Create a new MemPool object
 	pool := &MemPool{
-		pool:      make(map[string]interface{}),
-		mutex:     sync.Mutex{},
-		size:      0,
-		sizelimit: poolsize,
-		events:    false,
+		pool:  make(map[string]interface{}),
+		mutex: sync.Mutex{},
+		Count: 0,
+		size:  poolsize,
 	}
 
 	// Check if the event handler should be enabled
-	if eventenable {
+	if enable_events {
 		// Initialize the event handler
 		pool.eventchan = make(chan PoolEvent)
-		pool.events = true
 	}
 
 	// Return the new pool
