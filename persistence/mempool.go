11a1b8e2535fe98f73713da2d8c5f184eebb14e0
Manish Meganathan <meganathan.manish@gmail.com>
2021-08-05 23:38:43 +0530

    Add MemPool struct
    - Added a simple struct MemPool in mempool.go
    - Implements the fields for the memory pool such as its size, the actual pool, a sync lock and so on.
    - Added a constructor for the memory pool struct
    - Working towards #9

--- persistence/mempool.go
+++ persistence/mempool.go
@@ -1,5 +1,7 @@
 package persistence
 
+import "sync"
+
 // A type alias that represents a type of pool state event
 type PoolEvent uint
 
@@ -14,3 +16,44 @@ const (
 	// Pool has been resized
 	POOLRESIZE PoolEvent = 3
 )
+
+// A struct that represents a memory pool of objects
+type MemPool struct {
+	// Represents the actual addressable pool of objects
+	pool map[string]interface{}
+	// Represents the syncrhonization lock for the pool
+	mutex sync.Mutex
+
+	// Represents the current size of the pool
+	size uint
+	// Represents the maximum size of the pool
+	sizelimit uint
+
+	// Represents the event handler chanel for pool state events
+	eventchan chan PoolEvent
+	// Represents whether the the event handler is enabled.
+	events bool
+}
+
+// A constructor function that creates a new memory pool of the given size and returns it.
+// Requires a boolean that determines if the event handler for the pool should be initialized.
+func NewMemPool(poolsize uint, eventenable bool) *MemPool {
+	// Create a new MemPool object
+	pool := &MemPool{
+		pool:      make(map[string]interface{}),
+		mutex:     sync.Mutex{},
+		size:      0,
+		sizelimit: poolsize,
+		events:    false,
+	}
+
+	// Check if the event handler should be enabled
+	if eventenable {
+		// Initialize the event handler
+		pool.eventchan = make(chan PoolEvent)
+		pool.events = true
+	}
+
+	// Return the new pool
+	return pool
+}
